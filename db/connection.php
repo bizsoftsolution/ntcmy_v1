5522f3c4775b02884dd84d33254c13365fa6c156
Raj Kumar <rajkumar.bizsoft@gmail.com>
2018-07-16 14:43:18 +0530

    connecttionupdate

--- db/connection.php
+++ db/connection.php
@@ -1,8 +1,8 @@
 <?php
    // $conn=mysql_connect('localhost','root','toor');
    // mysql_select_db('nubehotel')or die(mysql_error());
-$conn=mysql_connect('localhost','ntcmalay_nubehot','test123!@#');
-   mysql_select_db('ntcmalay_nubehotel')or die(mysql_error());
+   $conn=mysql_connect('ntc.my','ntcmy_v1','BizSoft2018+');
+   mysql_select_db('ntcmy_v1')or die(mysql_error());
 
    // if(! $conn )
    // {
